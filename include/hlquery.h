893590f96a05ff5cbf9e3eaea090bcde835a7f41
Carlos <carlos.ferry@gmail.com>
2025-08-23 08:51:21 -0400

    Update configuration to correct committer email mismatch in GPG signatures.

---
+++ include/hlquery.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+#include <memory>
+#include <thread>
+#include <atomic>
+#include <chrono>
+#include <ctime>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <getopt.h>
+
+/* Global server instance pointer */
+
+extern hlquery* Instance;
+
+
+class hlquery
+{
+  public:
+  
+};
