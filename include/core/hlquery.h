be972571b3e2b131ea147da63cded7f11be01caf
Carlos <carlos.ferry@gmail.com>
2025-12-22 20:25:42 -0300

    Initialize hlquery instance and bootstrap ServerConfig from CLI args

---
+++ include/core/hlquery.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <chrono>
+#include <csignal>
+#include <ctime>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+
+extern hlquery* Instance;
+
+class  hlquery {
+
+ public:
+
+};
---
+++ src/core/hlquery.cpp
@@ -0,0 +1,26 @@
+#include <chrono>
+#include <csignal>
+#include <exception>
+#include <filesystem>
+#include <fstream>
+#include <future>
+#include <iostream>
+#include <random>
+#include <sys/resource.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <thread>
+#include <unistd.h>
+#include <vector>
+
+#include "core/hlquery.h"
+
+hlquery* Instance = nullptr;
+
+hlquery::hlquery(int argc, char** argv)
+{
+        Instance = this;
+        Config = std::make_unique<ServerConfig>(argc, argv);
+        ParseArgs();
+}
