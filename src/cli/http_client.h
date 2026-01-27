6c9371222012abbece997686e5503194fc7b1c83
Carlos <carlos.ferry@gmail.com>
2026-01-27 17:48:07 -0300

    Add 2 new function(s) or class(es) in hlquery_cli_main.cpp and http_client.h.

--- src/cli/http_client.h
+++ src/cli/http_client.h
@@ -1,13 +1,85 @@
+/*
+ * hlquery - Search beyond keywords.
+ * http://www.hlquery.com
+ *
+ * Copyright (C) 2021-2026, Carlos F. Ferry <carlos.ferry@gmail.com>
+ *
+ * This file is part of hlquery, released under the BSD License version 3.
+ * You are free to redistribute and/or modify this software
+ * under the terms of the BSD License.
+ * For more details, please visit: https://docs.hlquery.com
+ */
+
 #pragma once
 
 #include <map>
 #include <string>
 
-struct HTTPResponse {
-    int status_code;
-    std::string status_text;
-    std::map<std::string, std::string> headers;
-    std::string body;
+namespace hlquery_cli 
+{
+     /* HTTPResponse struct represents a response from the server. */
 
-    HTTPResponse() : status_code(-1) {}
-};
+     struct HTTPResponse 
+     {
+          int StatusCode;
+
+          std::string StatusText;
+
+          std::map<std::string, std::string> Headers;
+
+          std::string Body;
+        
+          /* Constructor for HTTPResponse. */
+
+          HTTPResponse()
+               : StatusCode(-1)
+          {
+     
+          }
+     };
+
+     /* HTTPClient class for making requests. */
+
+     class HTTPClient 
+     {
+          public:
+    
+               /* Constructor for HTTPClient. */
+
+               HTTPClient(const std::string& base_url, bool raw_mode = false, const std::string& auth_token = "");
+        
+               /* Makes a request to the server. */
+
+               HTTPResponse MakeRequest(const std::string& method, const std::string& path, const std::string& body = "");
+        
+               /* Sets raw mode. */
+
+               void SetRawMode(bool raw) 
+               {
+                    RawMode = raw;
+               }
+
+               /* Sets auth token. */
+
+               void SetAuthToken(const std::string& token) 
+               {
+                    AuthToken = token;
+               }
+        
+          private:
+    
+               std::string BaseURL;
+
+               std::string Host;
+
+               int Port;
+
+               bool RawMode;
+
+               std::string AuthToken;
+        
+               /* Parses a URL. */
+
+               void ParseURL(const std::string& url);
+     };
+}
